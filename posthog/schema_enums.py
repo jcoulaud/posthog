a055e427be8243ef8e87e4c729a244f0f3700814
Tom Owers <owerstom@gmail.com>
2026-07-30 12:06:57 +0000

    chore(data-warehouse): regenerate the schema artifacts for instagram

--- posthog/schema_enums.py
+++ posthog/schema_enums.py
@@ -2825,6 +2825,7 @@ class IntegrationKind(StrEnum):
     GITHUB = "github"
     GITLAB = "gitlab"
     META_ADS = "meta-ads"
+    INSTAGRAM = "instagram"
     CLICKUP = "clickup"
     REDDIT_ADS = "reddit-ads"
     DATABRICKS = "databricks"
