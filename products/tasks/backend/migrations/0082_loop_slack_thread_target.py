a4df52aca37fc365aef08346da10ef7916ffd8d7
Dylan Martin <dmarticus@gmail.com>
2026-08-03 15:24:29 -0700

    chore(tasks): format the slack_thread_target migration
    Generated-By: PostHog Code
    Task-Id: ff00c589-fb84-4c9c-ae85-ee60f14c4b27

--- products/tasks/backend/migrations/0082_loop_slack_thread_target.py
+++ products/tasks/backend/migrations/0082_loop_slack_thread_target.py
@@ -4,13 +4,12 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [("tasks", "0081_channelcontextgeneration_channelinstructions_and_more")]
 
     operations = [
         migrations.AddField(
-            model_name='loop',
-            name='slack_thread_target',
+            model_name="loop",
+            name="slack_thread_target",
             field=models.JSONField(blank=True, default=dict),
         ),
     ]
